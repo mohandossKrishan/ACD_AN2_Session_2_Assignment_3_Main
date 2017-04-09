16bed1ce12e31d86879c8d4361dd10374e8743cf
mohandossKrishan <mohandoss28@gmail.com>
2017-04-09 16:09:58 +0530

    first commit

---
+++ app/src/main/java/com/example/test/acd_an2_session_2_assignment_3_main/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.test.acd_an2_session_2_assignment_3_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/test/acd_an2_session_2_assignment_3_main/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.test.acd_an2_session_2_assignment_3_main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
